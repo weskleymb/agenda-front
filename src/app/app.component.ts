cac3efaf70f2f600045599219837f9a10734a70f
Weskley Bezerra <weskleymb@hotmail.com>
2019-10-10 19:45:04 -0300

    heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { ContatoService } from './contato.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 class Contato {
@@ -13,26 +13,33 @@ class Contato {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  ultimoId = 3;
   contato = new Contato();
-
   contatos = [];
 
   constructor(private service: ContatoService) {}
 
+  ngOnInit(): void {
+    this.buscaContatos();
+  }
+
+  buscaContatos() {
+    this.service.buscaTodos().then(dados => {
+      this.contatos = dados;
+    });
+  }
+
   salvar(formContato: NgForm) {
-    /*
-    this.contato.id = ++this.ultimoId;
     this.contato.nome = formContato.value.nome;
     this.contato.fone = formContato.value.fone;
 
-    this.contatos.push(this.contato);
+    this.service.salvaContato(this.contato)
+      .then(c => {
+        this.buscaContatos();
+      });
 
     this.contato = new Contato();
-    */
-    console.log(this.service.buscaTodos());
   }
 
   editar(id: number) {
--- src/app/contato.service.ts
+++ src/app/contato.service.ts
@@ -4,12 +4,18 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class ContatoService {
 
-  private const URL_BUSCA_TODOS = `http://localhost:8080/contatos-ws`;
+  private readonly URL_BUSCA_TODOS = `http://localhost:8080/contatos-ws`;
+  private readonly URL_SALVA_CONTATO = `http://localhost:8080/contatos-ws`;
 
   constructor(private http: HttpClient) {}
 
   buscaTodos(): Promise<any> {
     return this.http.get(this.URL_BUSCA_TODOS)
+                  .toPromise();
+  }
+
+  salvaContato(contato: any): Promise<any> {
+    return this.http.post(this.URL_SALVA_CONTATO, contato)
                   .toPromise();
   }
 
